12ee5320f1632c13cdc90af3a6e42648b4e031b2
JuanSuppicich <54631278+JuanSuppicich@users.noreply.github.com>
2021-07-31 15:47:57 -0300

    Revert "Feature UI trucks (#3)" (#4)
    This reverts commit 2025ec4b9a9e06093abb6e17f8d68da52159d559.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import { ChakraProvider, CSSReset } from '@chakra-ui/react';
-import CustomerHome from './components/customer/home/CustomerHome';
-import EmployeeHome from './components/employee/home/EmployeeHome';
-import EmployeeForm from './components/employee/EmployeeForm';
-import Login from './components/login/Login';
-import TruckForm from './components/truck/TruckForm';
-import Truck from './components/truck/Truck';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <ChakraProvider>
-      <CSSReset />
-      <Truck></Truck>
-    </ChakraProvider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
+
 export default App;
